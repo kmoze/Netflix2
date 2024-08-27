466cf310b4a7b099f68394b3bfcf686002659c7b
Joshua <joshuaisaact@gmail.com>
2024-08-27 09:27:48 +0100

    first draft

---
+++ src/components/Movie.jsx
@@ -0,0 +1,9 @@
+function Movie() {
+  return (
+    <a>
+      <img />
+    </a>
+  )
+}
+
+export default Movie
---
+++ src/components/Movielist.jsx
@@ -0,0 +1,13 @@
+import Movie from "./Movie"
+
+function Movielist() {
+  return (
+    <div>
+      <ul>
+        <li><Movie/></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Movielist
